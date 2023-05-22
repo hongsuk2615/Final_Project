f683a3634a9ccbaadc300b456c35b6c1ceeb08b6
minjaeee24 <ghkswo0624@naver.com>
2023-05-22 15:10:49 +0900

    d
    d

--- src/main/java/com/ace/thrifty/co_purchase/controller/co_purchaseController.java
+++ src/main/java/com/ace/thrifty/co_purchase/controller/co_purchaseController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class co_purchaseController {
 
-//	@RequestMapping("/home")
-//	public String home() {
-//		return "boardTemplate";
-//	}
 	@RequestMapping("/main.co")
 	public String usedProduct() {
 		return "co_purchase/purchaseMain";
