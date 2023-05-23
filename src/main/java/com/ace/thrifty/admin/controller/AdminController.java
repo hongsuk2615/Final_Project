bde1c3ffc9b39d6811fc00de9ecf82106678ec06
JunHa1307 <hshwan0406@gmail.com>
2023-05-23 15:17:05 +0900

    admin

--- src/main/java/com/ace/thrifty/admin/controller/AdminController.java
+++ src/main/java/com/ace/thrifty/admin/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.ace.thrifty.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,27 +10,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
 	@GetMapping("/home")
-	public String adminHome() {
-		return "admin/adminHome";
+	public String adminHome(Model model) {
+		model.addAttribute("sidebar", "home");
+		return "admin/adminPage";
 	}
 	@GetMapping("/member")
-	public String adminMember() {
-		return "admin/adminMember";
+	public String adminMember(Model model) {
+		model.addAttribute("sidebar", ".sidebar-member");
+		return "admin/adminPage";
 	}
 	@GetMapping("/report")
-	public String adminReport() {
-		return "admin/adminReport";
+	public String adminReport(Model model) {
+		model.addAttribute("sidebar", ".sidebar-report");
+		return "admin/adminPage";
 	}
 	@GetMapping("/board")
-	public String AdminBoard() {
-		return "admin/adminBoard";
+	public String AdminBoard(Model model) {
+		model.addAttribute("sidebar", ".sidebar-board");
+		return "admin/adminPage";
 	}
 	@GetMapping("/notice")
-	public String AdminNotice() {
-		return "admin/adminNotice";
+	public String AdminNotice(Model model) {
+		model.addAttribute("sidebar", ".sidebar-notice");
+		return "admin/adminPage";
 	}
 	@GetMapping("/faq")
-	public String Adminfaq() {
-		return "admin/adminFaq";
+	public String Adminfaq(Model model) {
+		model.addAttribute("sidebar", ".sidebar-faq");
+		return "admin/adminPage";
 	}
 }
---
+++ src/main/webapp/WEB-INF/views/admin/adminPage.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Starter</title>
+  <jsp:include page="adminHead.jsp" />
+
+</head>
+<body class="hold-transition sidebar-mini">
+	<div class="wrapper">
+		<jsp:include page="adminHeader.jsp" />
+		<jsp:include page="adminSidebar.jsp" />
+		<c:choose>
+			<c:when test="${sidebar eq 'home'}">
+				<jsp:include page="adminHomeContent.jsp" />
+			</c:when>
+			<c:when test="${sidebar eq '.sidebar-member'}">
+				<jsp:include page="adminMemberContent.jsp" />
+			</c:when>
+			<c:when test="${sidebar eq '.sidebar-report'}">
+				<jsp:include page="adminHomeContent.jsp" />
+			</c:when>
+			<c:when test="${sidebar eq '.sidebar-board'}">
+				<jsp:include page="adminHomeContent.jsp" />
+			</c:when>
+			<c:when test="${sidebar eq '.sidebar-notice'}">
+				<jsp:include page="adminHomeContent.jsp" />
+			</c:when>
+			<c:when test="${sidebar eq '.sidebar-faq'}">
+				<jsp:include page="adminHomeContent.jsp" />
+			</c:when>
+		</c:choose>
+		<jsp:include page="adminControllSidebar.jsp" />
+		<jsp:include page="adminFooter.jsp" />
+	</div>
+	<jsp:include page="adminJs.jsp" />
+	<script>
+		<c:if test="${sidebar ne 'home' }">
+			$("${sidebar}").children("a").addClass("active");
+		</c:if>
+	</script>
+</body>
+</html>
