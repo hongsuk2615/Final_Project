49b5414d333a0264fd7576db22ffea24364f9528
hongsuk2615 <hongsuk2615@gmail.com>
2023-05-29 11:39:50 +0900

    회원가입

--- src/main/java/com/ace/thrifty/member/model/service/MemberService.java
+++ src/main/java/com/ace/thrifty/member/model/service/MemberService.java
@@ -4,4 +4,8 @@ import com.ace.thrifty.member.model.vo.Member;
 
 public interface MemberService {
 	public Member selectMember();
+
+	public int insertMember(Member m);
+	
+	public Member loginMember(Member m);
 }
