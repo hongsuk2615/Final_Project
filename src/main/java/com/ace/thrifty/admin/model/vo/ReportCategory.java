451b4446df6fc0281d96d97707ad5ee551194317
SSU <113297372+SSHwan22@users.noreply.github.com>
2023-06-12 20:02:53 +0900

    ing

--- src/main/java/com/ace/thrifty/admin/model/vo/ReportCategory.java
+++ src/main/java/com/ace/thrifty/admin/model/vo/ReportCategory.java
@@ -1,5 +1,13 @@
 package com.ace.thrifty.admin.model.vo;
 
-public class ReportCategory {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ReportCategory {
+	private int reportCategoryNo;
+	private String reportCategoyName;
 }
