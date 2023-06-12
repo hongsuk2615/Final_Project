303ddf0c276d0af1ce4291c2d582eb32f3391828
hongsuk2615 <hongsuk2615@gmail.com>
2023-06-13 08:03:08 +0900

    비밀번호찾기

--- src/main/java/com/ace/thrifty/common/Utils.java
+++ src/main/java/com/ace/thrifty/common/Utils.java
@@ -4,6 +4,21 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,4 +37,84 @@ public class Utils {
 		
 		return changeName;
 	}
+	
+	public static void sendNewPwd(String userEmail, String newPwd) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("@gmail.com", "");
+			}
+		});
+
+		String receiver = userEmail; 
+		String title = "임시 비밀번호입니다";
+		String content = "임시 비밀번호 : "+newPwd;
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("@gmail.com", "관리자", "utf-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(content, "text/html; charset=utf-8");
+
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static String getRamdomPassword(int len) {
+		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+				'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+		int idx = 0;
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < len; i++) {
+
+			idx = (int) (charSet.length * Math.random()); // 36 * 생성된 난수를 Int로 추출 (소숫점제거)
+
+			sb.append(charSet[idx]);
+		}
+
+		return sb.toString();
+	}
+	
+	public static void sendNewCode(String userEmail, String Code ) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+		Session session = Session.getInstance(props, new Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("", "");
+			}
+		});
+
+		String receiver = userEmail; 
+		String title = "인증코드입니다.";
+		String content = "인증코드 : "+Code;
+		Message message = new MimeMessage(session);
+		try {
+			message.setFrom(new InternetAddress("", "관리자", "utf-8"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+			message.setSubject(title);
+			message.setContent(content, "text/html; charset=utf-8");
+
+			Transport.send(message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }	
