7556c743751cb6430efa471f8b0e89fd01e82341
hongsuk2615 <hongsuk2615@gmail.com>
2023-06-18 19:11:27 +0900

    쪽지함

--- src/main/java/com/ace/thrifty/message/controller/MessageController.java
+++ src/main/java/com/ace/thrifty/message/controller/MessageController.java
@@ -1,5 +1,9 @@
 package com.ace.thrifty.message.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.ace.thrifty.member.model.vo.Member;
 import com.ace.thrifty.message.model.service.MessageService;
 import com.ace.thrifty.message.model.vo.Message;
+import com.google.gson.Gson;
 
 @RequestMapping("/message")
 @Controller
@@ -28,7 +33,29 @@ public class MessageController {
 		} else {
 			int sender = ((Member) session.getAttribute("loginUser")).getUserNo();
 			message.setSender(sender);
+			System.out.println(message);
 			return messageService.insertMessage(message);
 		}
 	}
+	
+	@GetMapping("/selectUsers")
+	@ResponseBody
+	public String selectUsers(HttpSession session) {
+		int userNo = ((Member)session.getAttribute("loginUser")).getUserNo();
+		List<Member> users = messageService.selectUsers(userNo);
+		return new Gson().toJson(users);
+	}
+	
+	@GetMapping("/selectMessage")
+	@ResponseBody
+	public String selectMessage(HttpSession session, String userNo) {
+		int loginUserNo = ((Member)session.getAttribute("loginUser")).getUserNo();
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("loginUser", loginUserNo);
+		map.put("opponent", Integer.parseInt(userNo));
+		List<Message> list = messageService.selectMessage(map);
+		return new Gson().toJson(list);
+	}
+	
+	
 }
